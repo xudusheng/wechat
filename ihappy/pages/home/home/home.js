4ce8e28cdc466dc0cd7c7e44fd20fcf295ac06ee
xudusheng <xudusheng@bisinuolan.com>
2018-11-13 18:58:06 +0800

    更新：新建工程项目

---
+++ ihappy/pages/home/home/home.js
@@ -0,0 +1,71 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title:"首页",
+    goodsList:[
+      { goods_id: "123456", goods_title: "iPhone XS", goods_price: "8688",goods_icon: "https://img.bisinuolan.cn/8d0a69c7-0a8d-48bd-a9ef-66f1e7abec48-1541731149960.png", status: 1 },
+      { goods_id: "123456", goods_title: "Android XS", goods_price: "8688", goods_icon: "https://img.bisinuolan.cn/8d0a69c7-0a8d-48bd-a9ef-66f1e7abec48-1541731149960.png", status: 2 },
+      { goods_id: "123456", goods_title: "iPhone XS", goods_price: "8688", goods_icon: "https://img.bisinuolan.cn/8d0a69c7-0a8d-48bd-a9ef-66f1e7abec48-1541731149960.png", status: 3 },
+      { goods_id: "123456", goods_title: "Google XS", goods_price: "8688", goods_icon: "https://img.bisinuolan.cn/8d0a69c7-0a8d-48bd-a9ef-66f1e7abec48-1541731149960.png", status: 1 },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
